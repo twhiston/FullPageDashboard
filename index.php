fbe780a87c24b516640b5a09421cb6d1f984d810
Tom Whiston <tom.whiston@pwc-digital.ch>
2017-03-02 22:56:15 +0100

    turn off dev mode

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use twhiston\FullPageDashboard\FullPageDashboard;
 
-$app = new FullPageDashboard(TRUE);
+$app = new FullPageDashboard();//Pass in true if you want dev mode (nocache)
 $app->registerServices();
 $app->registerRoutes();
 $app->run();
